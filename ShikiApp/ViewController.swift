f7412aa1a93efbcd4d307fa4fa54dbaf4322c873
speaker378 <speaker378debua@gmail.com>
2023-01-15 18:56:05 +0500

    Initial commit

---
+++ ShikiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShikiApp
+//
+//  Created by Сергей Черных on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
