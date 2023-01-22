eac62b5d2379a1484de345da24f3c545b84fb45c
KonstantinShmondrik <shmond@mail.ru>
2023-01-22 15:48:54 +0600

    refactoring naming

--- ShikiApp/Core/Helpers/AppImage.swift
+++ ShikiApp/Core/Helpers/AppImage.swift
@@ -12,43 +12,43 @@ import UIKit
 
 enum AppImage {
     
-    private static let missingImege = UIImage()
+    private static let missingImage = UIImage()
     
     // MARK: - NavigationsBarIcons
     enum NavigationsBarIcons {
-        static let add = UIImage(named: "add") ?? missingImege
-        static let back = UIImage(named: "back") ?? missingImege
-        static let cancel = UIImage(named: "cancel") ?? missingImege
-        static let chevronBigRight = UIImage(named: "chevronBigRight") ?? missingImege
-        static let chevronDown = UIImage(named: "chevronDown") ?? missingImege
-        static let done = UIImage(named: "done") ?? missingImege
-        static let edit = UIImage(named: "edit") ?? missingImege
-        static let login = UIImage(named: "login") ?? missingImege
-        static let logout = UIImage(named: "logout") ?? missingImege
-        static let search = UIImage(named: "search_nav") ?? missingImege
-        static let share = UIImage(named: "share") ?? missingImege
+        static let add = UIImage(named: "add") ?? missingImage
+        static let back = UIImage(named: "back") ?? missingImage
+        static let cancel = UIImage(named: "cancel") ?? missingImage
+        static let chevronBigRight = UIImage(named: "chevronBigRight") ?? missingImage
+        static let chevronDown = UIImage(named: "chevronDown") ?? missingImage
+        static let done = UIImage(named: "done") ?? missingImage
+        static let edit = UIImage(named: "edit") ?? missingImage
+        static let login = UIImage(named: "login") ?? missingImage
+        static let logout = UIImage(named: "logout") ?? missingImage
+        static let search = UIImage(named: "search_nav") ?? missingImage
+        static let share = UIImage(named: "share") ?? missingImage
     }
     
     // MARK: - ErrorsIcons
     enum ErrorsIcons {
-        static let nonConnectionIcon = UIImage(named: "noConnectionIcon") ?? missingImege
-        static let noUserpicIcon = UIImage(named: "noUserpic") ?? missingImege
+        static let nonConnectionIcon = UIImage(named: "noConnectionIcon") ?? missingImage
+        static let noUserpicIcon = UIImage(named: "noUserpic") ?? missingImage
     }
     
     // MARK: - TabBarIcons
     enum TabBarIcons {
-        static let home = UIImage(named: "home") ?? missingImege
-        static let homeSelected = UIImage(named: "homeSelected") ?? missingImege
-        static let myList = UIImage(named: "myList") ?? missingImege
-        static let myListSelected = UIImage(named: "myListSelected") ?? missingImege
-        static let profile = UIImage(named: "profile") ?? missingImege
-        static let profileSelected = UIImage(named: "profileSelected") ?? missingImege
-        static let search = UIImage(named: "search") ?? missingImege
-        static let searchSelected = UIImage(named: "searchSelected") ?? missingImege
+        static let home = UIImage(named: "home") ?? missingImage
+        static let homeSelected = UIImage(named: "homeSelected") ?? missingImage
+        static let myList = UIImage(named: "myList") ?? missingImage
+        static let myListSelected = UIImage(named: "myListSelected") ?? missingImage
+        static let profile = UIImage(named: "profile") ?? missingImage
+        static let profileSelected = UIImage(named: "profileSelected") ?? missingImage
+        static let search = UIImage(named: "search") ?? missingImage
+        static let searchSelected = UIImage(named: "searchSelected") ?? missingImage
     }
     
     enum OtherIcons {
-        static let link = UIImage(named: "link") ?? missingImege
+        static let link = UIImage(named: "link") ?? missingImage
     }
     
 }
