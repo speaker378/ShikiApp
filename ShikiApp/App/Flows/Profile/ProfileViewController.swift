35d58ef288f7713a038bd3cbf4671176ea037812
AntonLebedev <lebedev_anton@mail.ru>
2023-01-30 19:50:40 +0300

    Первый раз присылаю код на ревью. Не нашёл у нас в библиотеке цветов, какой красный цвет (он нужен для лейбла "Выход из приложения). Bottom2 - это замена Bottom из NewsfeedTableViewCell.

--- ShikiApp/App/Flows/Profile/ProfileViewController.swift
+++ ShikiApp/App/Flows/Profile/ProfileViewController.swift
@@ -10,34 +10,35 @@ import UIKit
 class ProfileViewController: UIViewController {
     
     var userUrlString: String = ""
-    
+
     // MARK: - Properties
+    
     // Canvas view
     lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemBackground
         
-        view.addSubview(topGrayDivider)
-        topGrayDivider.backgroundColor = .systemGray6
-        topGrayDivider.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 1)
+        view.addSubview(topDivider)
+        topDivider.backgroundColor = .systemGray6
+        topDivider.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 1)
         
-        view.addSubview(profileImageView)
-        profileImageView.anchor(top: topGrayDivider.bottomAnchor, left: view.leftAnchor, paddingTop: 20, paddingLeft: 16, width: 100, height: 100)
+        view.addSubview(profileImgView)
+        profileImgView.anchor(top: topDivider.bottomAnchor, left: view.leftAnchor, paddingTop: 20, paddingLeft: 16, width: 100, height: 100)
         
         view.addSubview(nameLabel)
-        nameLabel.anchor(top: topGrayDivider.bottomAnchor, left: profileImageView.rightAnchor, paddingTop: 32, paddingLeft: 8)
+        nameLabel.anchor(top: topDivider.bottomAnchor, left: profileImgView.rightAnchor, paddingTop: 32, paddingLeft: 8)
         
         view.addSubview(sexLabel)
-        sexLabel.anchor(top: nameLabel.bottomAnchor, left: profileImageView.rightAnchor, paddingTop: 5, paddingLeft: 8)
+        sexLabel.anchor(top: nameLabel.bottomAnchor, left: profileImgView.rightAnchor, paddingTop: 5, paddingLeft: 8)
         
         view.addSubview(ageLabel)
         ageLabel.anchor(top: nameLabel.bottomAnchor, left: sexLabel.rightAnchor, paddingTop: 5, paddingLeft: 5)
         
-        view.addSubview(linkImageView)
-        linkImageView.anchor(top: profileImageView.bottomAnchor, left: view.leftAnchor, paddingTop: 12, paddingLeft: 15)
+        view.addSubview(linkImgView)
+        linkImgView.anchor(top: profileImgView.bottomAnchor, left: view.leftAnchor, paddingTop: 12, paddingLeft: 15)
         
-        view.addSubview(websiteButton)
-        websiteButton.anchor(top: profileImageView.bottomAnchor, left: linkImageView.rightAnchor, paddingTop: 7, paddingLeft: 5)
+        view.addSubview(webButton)
+        webButton.anchor(top: profileImgView.bottomAnchor, left: linkImgView.rightAnchor, paddingTop: 7, paddingLeft: 5)
         
         view.addSubview(logoutButton)
         logoutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -47,26 +48,20 @@ class ProfileViewController: UIViewController {
         versionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         versionLabel.anchor(top: logoutButton.bottomAnchor, paddingTop: 5)
         
-        view.addSubview(bottomGrayDivider)
-        bottomGrayDivider.backgroundColor = .systemGray6
-        bottomGrayDivider.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor,  paddingBottom: 1, height: 1)
-        
         return view
     }()
     
-    let topGrayDivider: UIView = {
+    let topDivider: UIView = {
         let dividerView = UIView()
         dividerView.backgroundColor = .black
         return dividerView
     }()
     
     
-    let profileImageView: UIImageView = {
+    let profileImgView: UIImageView = {
         let imageView = UIImageView()
-        imageView.image = UIImage(systemName: "person.circle")
+        imageView.image = AppImage.ErrorsIcons.noUserpicIcon
         imageView.tintColor = UIColor.systemGray3
-        //Circle cut
-        //imageView.contentMode = .scaleAspectFit
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 50
         imageView.clipsToBounds = true
@@ -76,35 +71,35 @@ class ProfileViewController: UIViewController {
     let nameLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
-        label.font = UIFont.boldSystemFont(ofSize: 26)
-        label.textColor = .darkGray
+        label.font = AppFont.openSansFont(ofSize: 26, weight: .regular)
+        label.textColor = AppColor.textMain
         return label
     }()
     
     let sexLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
-        label.font = UIFont.boldSystemFont(ofSize: 14)
-        label.textColor = .systemGray2
+        label.font = AppFont.openSansFont(ofSize: 14, weight: .regular)
+        label.textColor = AppColor.textMinor
         return label
     }()
     
     let ageLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .left
-        label.font = UIFont.boldSystemFont(ofSize: 14)
-        label.textColor = .systemGray2
+        label.font = AppFont.openSansFont(ofSize: 14, weight: .regular)
+        label.textColor = AppColor.textMinor
         return label
     }()
     
-    let linkImageView: UIImageView = {
+    let linkImgView: UIImageView = {
         let imageView = UIImageView()
-        imageView.tintColor = UIColor.systemGray2
-        imageView.image = UIImage(systemName: "link")
+        imageView.tintColor = AppColor.textMinor
+        imageView.image = AppImage.OtherIcons.link
         return imageView
     }()
     
-    let websiteButton: UIButton = {
+    let webButton: UIButton = {
         let button = UIButton(type: .system)
         button.titleLabel?.textAlignment = .left
         button.addTarget(ProfileViewController.self, action: #selector(handleUserWebpage), for: .touchUpInside)
@@ -116,29 +111,25 @@ class ProfileViewController: UIViewController {
         button.setTitle("Выйти из аккаунта", for: .normal)
         button.tintColor = .red
         button.titleLabel?.textAlignment = .center
-        //button.addTarget(ProfileViewController.self, action: #selector(handleLogout), for: .touchUpInside)
+        // button.addTarget(ProfileViewController.self, action: #selector(handleLogout), for: .touchUpInside)
         return button
     }()
     
     let versionLabel: UILabel = {
         let label = UILabel()
         label.text = "Версия 1.0.0 (1)"
-        label.font = UIFont.boldSystemFont(ofSize: 14)
-        label.textColor = .systemGray3
+        label.font = AppFont.openSansFont(ofSize: 14, weight: .regular)
+        label.textColor = AppColor.textMinor
         return label
     }()
     
-    let bottomGrayDivider: UIView = {
-        let dividerView = UIView()
-        return dividerView
-    }()
-    
+
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Профиль"
-        fetchProfile()
+        // fetchProfile()
         view.backgroundColor = .systemBackground
         
         view.addSubview(containerView)
@@ -146,30 +137,30 @@ class ProfileViewController: UIViewController {
         containerView.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: view.height/5, width: view.width, height: view.height/5*3.5)
     }
     
-    private func fetchProfile() {
-        APICaller.shared.getCurrentUserProfile { [weak self] result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let model):
-                    self?.updateUI(with: model)
-                case .failure(let error):
-                    print("Profile Error: \(error.localizedDescription)")
-                    self?.failedToGetProfile()
-                }
-            }
-        }
-    }
+    // private func fetchProfile() {
+    //    APICaller.shared.getCurrentUserProfile { [weak self] result in
+    //        DispatchQueue.main.async {
+    //            switch result {
+    //            case .success(let model):
+    //                self?.updateUI(with: model)
+    //            case .failure(let error):
+    //                print("Profile Error: \(error.localizedDescription)")
+    //                self?.failedToGetProfile()
+    //            }
+    //        }
+    //    }
+    // }
     
     private func updateUI(with model: UserProfileDTO) {
        
         if let imageUrl = URL(string: model.image?.x148 ?? "") {
-            profileImageView.loadImageFromURL(url: imageUrl)
+            profileImgView.loadImageFromURL(url: imageUrl)
         } else {
             return
         }
             
         nameLabel.text = model.nickname
-        websiteButton.setTitle(model.website, for: .normal)
+        webButton.setTitle(model.website, for: .normal)
         
         if let ageString = model.fullYears {
             ageLabel.text = String(ageString)
@@ -177,13 +168,16 @@ class ProfileViewController: UIViewController {
         
         
         if model.sex == "male" || model.sex == "female" {
-            if model.fullYears ?? 0 > 0 {
-            let sexValueInRussian = model.sex != "male" ? "женщина," : "мужчина,"
-            sexLabel.text = sexValueInRussian
-            } else {
-                let sexValueInRussian = model.sex != "male" ? "женщина" : "мужчина"
-                sexLabel.text = sexValueInRussian
+            if model.fullYears != nil {
+                guard let ageNumber = Int(model.fullYears ?? "0") else { return }
+                if ageNumber > 0 {
+                    let sexValueInRussian = model.sex != "male" ? "женщина," : "мужчина,"
+                    sexLabel.text = sexValueInRussian
+                } else {
+                    let sexValueInRussian = model.sex != "male" ? "женщина" : "мужчина"
+                    sexLabel.text = sexValueInRussian
                 }
+            }
         } else {
             return
         }
@@ -200,7 +194,7 @@ class ProfileViewController: UIViewController {
         view.addSubview(label)
         label.center = view.center
     }
-    
+
     // MARK: - Selectors
     
     @objc func handleUserWebpage() {
@@ -225,7 +219,7 @@ extension UIColor {
 
 // MARK: - enables layout without Storyboard. Good for use in future projects.
 extension UIView {
-    ///When we call anchor, we do not always input all params, hence optionals
+    
     func anchor(
         top: NSLayoutYAxisAnchor? = nil,
         left: NSLayoutXAxisAnchor? = nil,
