77b39f139b80c758ef113bc29ead68eacde3024d
speaker378 <speaker378debua@gmail.com>
2023-02-03 23:39:49 +0500

    getting images from the network on the newsfeed screen

--- ShikiApp/App/Entity/News/NewsModel.swift
+++ ShikiApp/App/Entity/News/NewsModel.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 struct NewsModel {
-    let image: UIImage?
+    let imageUrls: [ImageSize: String]
     let date: String?
     let title: String?
     let subtitle: String?
     let images: [UIImage?]
     let URLString: String?
+}
+
+enum ImageSize {
+    case original, preview, x96, x48
 }
 
 final class NewsModelFactory {
@@ -22,7 +26,7 @@ final class NewsModelFactory {
     }
     
     private func convertModel(from news: TopicDTO) -> NewsModel {
-        let image = AppImage.ErrorsIcons.nonConnectionIcon
+        let imageUrls = getImageSizes(for: news.linked?.image)
         let date = news
             .createdAt?
             .convertToDate()?
@@ -33,12 +37,22 @@ final class NewsModelFactory {
         let URLString = "\(Constants.Url.baseUrl)\(news.linked?.url ?? "")"
         
         return NewsModel(
-            image: image,
+            imageUrls: imageUrls,
             date: date,
             title: title,
             subtitle: subtitle,
             images: images,
             URLString: URLString
         )
+    }
+    
+    private func getImageSizes(for image: ImageDTO?) -> [ImageSize: String] {
+        guard let image else { return [:] }
+        return [
+            .original: "\(Constants.Url.baseUrl)\(image.original)",
+            .preview: "\(Constants.Url.baseUrl)\(image.preview)",
+            .x96: "\(Constants.Url.baseUrl)\(image.x96)",
+            .x48: "\(Constants.Url.baseUrl)\(image.x48)"
+        ]
     }
 }
