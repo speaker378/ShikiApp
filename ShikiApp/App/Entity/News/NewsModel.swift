077edbf44227a1e5a6aa48a196499d9db0843ee6
speaker378 <speaker378debua@gmail.com>
2023-02-05 22:35:14 +0500

    fixed getting video previews

--- ShikiApp/App/Entity/News/NewsModel.swift
+++ ShikiApp/App/Entity/News/NewsModel.swift
@@ -51,8 +51,15 @@ final class NewsModelFactory {
     private func extractImageAddresses(from footer: String?) -> [ImageSize: String] {
         var result = [ImageSize: String]()
         guard let footer else { return result }
-        let urls = footer.extractURLs()
-        let imageUrls = urls.filter { $0.contains(".jpg") }
+        let imageUrls = footer
+            .extractURLs()
+            .filter { $0.contains(".jpg") }
+        if (imageUrls.first(where: { $0.contains("youtube") }) != nil) {
+            let imgUrl = imageUrls.first(where: { $0.contains("youtube") })
+            result[.preview] = imgUrl
+            result[.original] = imgUrl
+            return result
+        }
         result[.preview] = imageUrls.first(where: { $0.contains("preview") })
         result[.original] = imageUrls.first(where: { $0.contains("original") })
         return result
