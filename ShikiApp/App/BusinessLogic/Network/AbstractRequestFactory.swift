0c946fca14db7894dcd74c87f7e36b35dc48bd54
speaker378 <speaker378debua@gmail.com>
2023-02-14 23:13:00 +0500

    change according to comments

--- ShikiApp/App/BusinessLogic/Network/AbstractRequestFactory.swift
+++ ShikiApp/App/BusinessLogic/Network/AbstractRequestFactory.swift
@@ -72,11 +72,11 @@ class AbstractRequestFactory<API: EndPointType>: AbstractRequestFactoryProtocol 
         }
     }
 
-    // MARK: - Private Functions
+    // MARK: - Private functions
     
     private func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
         switch response.statusCode {
-        case 200 ... 299: return .success("success")
+        case 200 ... 299: return .success(NetworkLayerErrorMessages.success)
         case 401 ... 500: return .failure(NetworkLayerErrorMessages.authenticationError)
         case 501 ... 599: return .failure(NetworkLayerErrorMessages.badRequest)
         case 600: return .failure(NetworkLayerErrorMessages.outdated)
