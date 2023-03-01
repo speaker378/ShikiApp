f7b69e1402b93861a879e953a04e8fad9f2af433
ads63 <48177421+ads63@users.noreply.github.com>
2023-03-01 15:56:21 +0300

    Change seasons list building function

--- ShikiApp/App/Entity/Filters/SeasonModel.swift
+++ ShikiApp/App/Entity/Filters/SeasonModel.swift
@@ -11,14 +11,21 @@ import Foundation
 
 struct SeasonModel {
 
+    // MARK: - Private properties
+
+    private let year: Int
+    private let season: String
+
     // MARK: - Properties
 
-    var description: String
+    var description: String { "\(season) \(year)" }
 
     // MARK: - Constructions
 
     init(season: String, year: Int) {
-        description = "\(season) \(year)"
+        self.year = year
+        self.season = season
+
     }
 }
 
@@ -26,22 +33,39 @@ struct SeasonModel {
 
 final class SeasonModelFactory {
 
+    // MARK: - Private properties
+
+    private let removeSeasonsByMonth = [
+        [0, 5],
+        [0, 5],
+        [1, 4],
+        [1, 4],
+        [1, 4],
+        [2, 3],
+        [2, 3],
+        [2, 3],
+        [3, 2],
+        [3, 2],
+        [3, 2],
+        [4, 1]
+    ]
+
     // MARK: - Functions
 
     func build() -> [SeasonModel] {
         var result: [SeasonModel] = []
-        guard let year = Calendar.current.dateComponents([.year], from: Date()).year else { return result }
-        let seasons = Seasons
-            .allCases
-            .enumerated()
-            .sorted(by: { $0.offset < $1.offset })
-            .map { $0.element.rawValue }
-
-        for yearOffset in 0 ... 1 {
+        guard
+            let currentYear = Calendar.current.dateComponents([.year], from: Date()).year,
+            let currentMonth = Calendar.current.dateComponents([.month], from: Date()).month
+        else { return result }
+        let seasons = Seasons.descriptions
+        for year in currentYear - 1 ... currentYear + 1 {
             for season in seasons {
-                result.append(SeasonModel(season: season, year: year - yearOffset))
+                result.append(SeasonModel(season: season, year: year))
             }
         }
-        return result
+        result.removeFirst(removeSeasonsByMonth[currentMonth - 1][0])
+        result.removeLast(removeSeasonsByMonth[currentMonth - 1][1])
+        return result.reversed()
     }
 }
